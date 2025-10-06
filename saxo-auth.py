21ac17fc1f0c9c8e35e1a7a8390fc1fc19b7d405
Joris <djm300@gmail.com>
2025-10-06 19:31:14 +0200

    saxo

--- saxo-auth.py
+++ saxo-auth.py
@@ -1,4 +1,7 @@
 import requests
+import base64
+import hashlib
+import os
 
 class OAuth2Client:
     def __init__(self, client_id, client_secret, redirect_uri, auth_endpoint, token_endpoint):
@@ -7,6 +10,7 @@ class OAuth2Client:
         self.redirect_uri = redirect_uri
         self.auth_endpoint = auth_endpoint
         self.token_endpoint = token_endpoint
+
     def _get_auth_url(self, **params):
         default_params = {
             'client_id': self.client_id,
@@ -14,13 +18,19 @@ class OAuth2Client:
         }
         default_params.update(params)
         return self.auth_endpoint + '?' + '&'.join(f"{k}={v}" for k, v in default_params.items())
-    def _exchange_for_token(self, code):
-        return requests.post(self.token_endpoint, data={
+
+    def _exchange_for_token(self, code, code_verifier):
+        response = requests.post(self.token_endpoint, data={
             'client_id': self.client_id,
             'client_secret': self.client_secret,
+            'grant_type': 'authorization_code',
             'code': code,
-            'redirect_uri': self.redirect_uri
-        }).json()
+            'redirect_uri': self.redirect_uri,
+            'code_verifier': code_verifier
+        })
+        print("Token endpoint response status code:", response.status_code)
+        print("Token endpoint response content:", response.content)
+        return response.json()
 
 
 def handle_oauth_errors(func):
@@ -32,19 +42,33 @@ def handle_oauth_errors(func):
     return wrapper
 
 class AuthorizationCodeClient(OAuth2Client):
+    def __init__(self, client_id, client_secret, redirect_uri, auth_endpoint, token_endpoint):
+        super().__init__(client_id, client_secret, redirect_uri, auth_endpoint, token_endpoint)
+        self.code_verifier = None
+        self.code_challenge = None
+
+    def _generate_code_verifier(self):
+        code_verifier = base64.urlsafe_b64encode(os.urandom(32)).decode('utf-8').rstrip('=')
+        self.code_verifier = code_verifier
+        return code_verifier
+
+    def _generate_code_challenge(self, code_verifier):
+        code_challenge = hashlib.sha256(code_verifier.encode('utf-8')).digest()
+        code_challenge = base64.urlsafe_b64encode(code_challenge).decode('utf-8').rstrip('=')
+        self.code_challenge = code_challenge
+        return code_challenge
+
     @handle_oauth_errors
     def get_authorization_url(self, **params):
+        code_verifier = self._generate_code_verifier()
+        code_challenge = self._generate_code_challenge(code_verifier)
+        params['code_challenge'] = code_challenge
+        params['code_challenge_method'] = 'S256'
         return self._get_auth_url(response_type='code', **params)
+
     @handle_oauth_errors
     def get_token(self, code):
-        return self._exchange_for_token(code)
-
-
-# Implicit flow
-class ImplicitClient(OAuth2Client):
-    @handle_oauth_errors
-    def get_authorization_url(self, **params):
-        return self._get_auth_url(response_type='token', **params)
+        return self._exchange_for_token(code, self.code_verifier)
 
 
 class ClientCredentialsClient(OAuth2Client):
@@ -58,7 +82,7 @@ class ClientCredentialsClient(OAuth2Client):
         return response.json()
 
 # Define your OAuth2 client
-client = ImplicitClient(
+client = AuthorizationCodeClient(
     client_id="c310e92ffc7c481190119ea98c507a2e",
     client_secret="67f8314ea810459e8ddc725a4cfd5568",
     redirect_uri="http://gttkeith.github.io/python-saxo/authcode",
@@ -72,5 +96,12 @@ print(f"Redirect the user to: {auth_url}")
 
 # After redirect, exchange the code for a token
 code = "RECEIVED_CODE"
-#token_info = client.get_token(code)
-#print(token_info)
+# The code verifier should be the same that was used to generate the code challenge
+# In a real application, you would store the code_verifier after generating the authorization URL
+# and retrieve it here. For this example, we'll simulate retrieving it from storage.
+# code_verifier = retrieve_code_verifier_from_storage()
+# For demonstration purposes, we'll generate a new one (THIS IS NOT SECURE IN A REAL APP):
+client._generate_code_verifier()
+token_info = client.get_token(code)
+print(token_info)
+
